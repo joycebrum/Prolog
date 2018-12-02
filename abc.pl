70a5d4fe4242af81ce2518e0c6437cdf24b0c1ce
Thiago <topd97@yahoo.com.br>
2018-12-02 17:35:56 -0200

    agora com conversor Port 2 P

--- abc.pl
+++ abc.pl
@@ -38,3 +38,12 @@ palavra([X|Xs],[C|D]) :- silaba(X,C), palavra(Xs,D).
 silaba(X, Rs) :- transforma(X, [C | Cs]), consoante(C), transforma(Ss, Cs), concatena(Ss, Qs), string_concat(X, Qs, Fs), atom_string(Rs, Fs).
 silaba(X, Rs)  :- transforma(X, [C | _Cs]), vogal(C), concatena(X, Qs), string_concat(X, Qs, Fs), atom_string(Rs,Fs).
 
+frasesP([],[]).
+frasesP([X|Xs],[A|B]) :- palavraP(X,A), frasesP(Xs,B).
+
+palavraP([],[]).
+palavraP([X|Xs],[C|D]) :- silabaP(X,C), palavraP(Xs,D).
+
+silabaP(X, []) :- transforma(X, [C | Cs]), C==p.
+
+silabaP(X, [C|Rs]) :- transforma(X, [C | Cs]), transforma(Cs,Ss), silabaP(Ss,Rs).
